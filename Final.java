432d1be6cc5f0d4dd3ad8fd728bfdfb94717bfc1
deepigarg <48362243+deepigarg@users.noreply.github.com>
2019-11-02 14:10:14 +0530

    A bit of animation

--- Final.java
+++ Final.java
@@ -1,8 +1,6 @@
 package sample;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
-import javafx.animation.Timeline;
+import javafx.animation.*;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -26,7 +24,7 @@ public class Main extends Application {
         b.setLayoutX(posx);
         b.setLayoutY(posy);
         b.setPrefSize(sx,sy);
-        //b.setStyle("-fx-background-color:transparent;");
+        b.setStyle("-fx-background-color:transparent;");
     }
 
     // launch the application
@@ -38,58 +36,83 @@ public class Main extends Application {
             Scene scene2 = new Scene(stackPaneG,1350,700);
 
             Pane pG = new Pane();
-            Image image1G = new Image("Lawn.png", 1330,700,false,false);
+            Image image1G = new Image("file:Lawn.png", 1330,700,false,false);
             ImageView iv1G = new ImageView();
             iv1G.setImage(image1G);
 
             Pane p2G = new Pane();
-            Image image2G = new Image("pauseMenu.png", 1350,800,false,false);
+            Image image2G = new Image("file:pauseMenu.png", 1350,800,false,false);
             ImageView iv2G = new ImageView();
             iv2G.setImage(image2G);
             iv2G.setPreserveRatio(true);
             p2G.getChildren().add(iv2G);
-            
 
-            Image zomimg = new Image("Zombie.gif",80,150,false,false);
-            ImageView ivzom = new ImageView();
-            ivzom.setImage(zomimg);
-            ivzom.setPreserveRatio(true);
-            ivzom.relocate(1135,40);
-            pG.getChildren().add(ivzom);
-
-            Image plimg = new Image("Pea.gif",90,110,false,false);
-            ImageView ivpl = new ImageView();
-            ivpl.setImage(plimg);
-            ivpl.setPreserveRatio(true);
-            ivpl.relocate(90,100);
-            pG.getChildren().add(ivpl);
-
-            Image lm1 = new Image("LawnMower.png", 100,70, false, false);
+            Image lm1 = new Image("file:LawnMower.png", 100,70, false, false);
             ImageView ivlm1 = new ImageView();
             ivlm1.setImage(lm1);
             ivlm1.setPreserveRatio(true);
-            ivlm1.relocate(10,115);
+            ivlm1.relocate(310,115);
             pG.getChildren().add(ivlm1);
             ImageView ivlm2 = new ImageView();
             ivlm2.setImage(lm1);
             ivlm2.setPreserveRatio(true);
-            ivlm2.relocate(10,230);
+            ivlm2.relocate(310,230);
             pG.getChildren().add(ivlm2);
             ImageView ivlm3 = new ImageView();
             ivlm3.setImage(lm1);
             ivlm3.setPreserveRatio(true);
-            ivlm3.relocate(10,335);
+            ivlm3.relocate(310,335);
             pG.getChildren().add(ivlm3);
             ImageView ivlm4 = new ImageView();
             ivlm4.setImage(lm1);
             ivlm4.setPreserveRatio(true);
-            ivlm4.relocate(10,450);
+            ivlm4.relocate(310,450);
             pG.getChildren().add(ivlm4);
             ImageView ivlm5 = new ImageView();
             ivlm5.setImage(lm1);
             ivlm5.setPreserveRatio(true);
-            ivlm5.relocate(10,565);
+            ivlm5.relocate(310,565);
             pG.getChildren().add(ivlm5);
+
+            Image pb = new Image("file:Peabullet.png",30,30,false,false);
+            ImageView ivpb = new ImageView();
+            ivpb.setImage(pb);
+            pG.getChildren().add(ivpb);
+            //Creating Translate Transition
+            TranslateTransition bullettrans = new TranslateTransition();
+            //Setting the duration of the transition
+            bullettrans.setDuration(Duration.seconds(10));
+            //Setting the node for the transition
+            bullettrans.setNode(ivpb);
+            //Setting the value of the transition along the x axis.
+            bullettrans.setFromX(415);
+            bullettrans.setFromY(130);
+            bullettrans.setToX(1300);
+            //Playing the animation
+
+
+            Image plimg = new Image("file:Pea.gif",100,110,false,false);
+            ImageView ivpl = new ImageView();
+            ivpl.setImage(plimg);
+            ivpl.setPreserveRatio(true);
+            ivpl.relocate(400,100);
+            pG.getChildren().add(ivpl);
+
+            Image zomimg = new Image("file:Zombie.gif",80,150,false,false);
+            ImageView ivzom = new ImageView();
+            ivzom.setImage(zomimg);
+            ivzom.setPreserveRatio(true);
+            ivzom.relocate(1155,60);
+            pG.getChildren().add(ivzom);
+            //Creating Translate Transition
+            TranslateTransition translateTransition = new TranslateTransition();
+            //Setting the duration of the transition
+            translateTransition.setDuration(Duration.seconds(38));
+            //Setting the node for the transition
+            translateTransition.setNode(ivzom);
+            //Setting the value of the transition along the x axis.
+            translateTransition.setByX(-900);
+            //Playing the animation
 
             Button save = new Button("Save");
             save.setLayoutX(500);
@@ -118,7 +141,7 @@ public class Main extends Application {
             //pauseMenu.setStyle("-fx-background-color:transparent;");
             exit.setOnAction(e->{
                 System.out.println("Exit to main menu selected");
-
+                stage.setScene(scene1);
             });
 
             Button resume = new Button("Resume");
@@ -128,7 +151,7 @@ public class Main extends Application {
             //pauseMenu.setStyle("-fx-background-color:transparent;");
             resume.setOnAction(e->{
                 System.out.println("Resume selected");
-                stackPane.getChildren().remove(p2G);
+                stackPaneG.getChildren().remove(p2G);
 
             });
 
@@ -140,99 +163,99 @@ public class Main extends Application {
             Label NumSuns = new Label("");
             NumSuns.setText("50");
             pG.getChildren().add(NumSuns);
-            NumSuns.setLayoutX(70);
-            NumSuns.setLayoutY(75);
+            NumSuns.setLayoutX(50);
+            NumSuns.setLayoutY(90);
 
             Button zz = new Button("0,0");
-            lawnbutton(zz,70,100,120,100);
+            lawnbutton(zz,380,100,100,100);
             Button zo = new Button("0,1");
-            lawnbutton(zo,70,215,120,100);
+            lawnbutton(zo,380,215,100,100);
             Button ztw = new Button("0,2");
-            lawnbutton(ztw,70,330,120,100);
+            lawnbutton(ztw,380,330,100,100);
             Button zth = new Button("0,3");
-            lawnbutton(zth,70,450,120,100);
+            lawnbutton(zth,380,450,100,100);
             Button zerof = new Button("0,4");
-            lawnbutton(zerof,70,560,120,100);
+            lawnbutton(zerof,380,560,100,100);
             Button oz = new Button("1,0");
-            lawnbutton(oz,200,100,120,100);
+            lawnbutton(oz,490,100,100,100);
             Button oo = new Button("1,1");
-            lawnbutton(oo,200,215,120,100);
+            lawnbutton(oo,490,215,100,100);
             Button otw = new Button("1,2");
-            lawnbutton(otw,200,330,120,100);
+            lawnbutton(otw,490,330,100,100);
             Button oth = new Button("1,3");
-            lawnbutton(oth,200,450,120,100);
+            lawnbutton(oth,490,450,100,100);
             Button of = new Button("1,4");
-            lawnbutton(of,200,560,120,100);
+            lawnbutton(of,490,560,100,100);
             Button twz = new Button("2,0");
-            lawnbutton(twz,340,100,120,100);
+            lawnbutton(twz,585,100,100,100);
             Button two = new Button("2,1");
-            lawnbutton(two,340,215,120,100);
+            lawnbutton(two,585,215,100,100);
             Button twtw = new Button("2,2");
-            lawnbutton(twtw,340,330,120,100);
+            lawnbutton(twtw,585,330,100,100);
             Button twth = new Button("2,3");
-            lawnbutton(twth,340,450,120,100);
+            lawnbutton(twth,585,450,100,100);
             Button twf = new Button("2,4");
-            lawnbutton(twf,340,560,120,100);
+            lawnbutton(twf,585,560,100,100);
             Button thz = new Button("3,0");
-            lawnbutton(thz,480,100,120,100);
+            lawnbutton(thz,687,100,100,100);
             Button tho = new Button("3,1");
-            lawnbutton(tho,480,215,120,100);
+            lawnbutton(tho,687,215,100,100);
             Button thtw = new Button("3,2");
-            lawnbutton(thtw,480,330,120,100);
+            lawnbutton(thtw,687,330,100,100);
             Button thth = new Button("3,3");
-            lawnbutton(thth,480,450,120,100);
+            lawnbutton(thth,687,450,100,100);
             Button thf = new Button("3,4");
-            lawnbutton(thf,480,560,120,100);
+            lawnbutton(thf,687,560,100,100);
             Button fz = new Button("4,0");
-            lawnbutton(fz,610,100,120,100);
+            lawnbutton(fz,790,100,100,100);
             Button fo = new Button("4,1");
-            lawnbutton(fo,610,215,120,100);
+            lawnbutton(fo,790,215,100,100);
             Button ftw = new Button("4,2");
-            lawnbutton(ftw,610,330,120,100);
+            lawnbutton(ftw,790,330,100,100);
             Button fth = new Button("4,3");
-            lawnbutton(fth,610,450,120,100);
+            lawnbutton(fth,790,450,100,100);
             Button ff = new Button("4,4");
-            lawnbutton(ff,610,560,120,100);
+            lawnbutton(ff,790,560,100,100);
             Button fiz = new Button("5,0");
-            lawnbutton(fiz,745,100,120,100);
+            lawnbutton(fiz,896,100,100,100);
             Button fio = new Button("5,1");
-            lawnbutton(fio,745,215,120,100);
+            lawnbutton(fio,896,215,100,100);
             Button fitw = new Button("5,2");
-            lawnbutton(fitw,745,330,120,100);
+            lawnbutton(fitw,896,330,100,100);
             Button fith = new Button("5,3");
-            lawnbutton(fith,745,450,120,100);
+            lawnbutton(fith,896,450,100,100);
             Button fif = new Button("5,4");
-            lawnbutton(fif,745,560,120,100);
+            lawnbutton(fif,896,560,100,100);
             Button sz = new Button("6,0");
-            lawnbutton(sz,875,100,120,100);
+            lawnbutton(sz,999,100,100,100);
             Button so = new Button("6,1");
-            lawnbutton(so,875,215,120,100);
+            lawnbutton(so,999,215,100,100);
             Button stw = new Button("6,2");
-            lawnbutton(stw,875,330,120,100);
+            lawnbutton(stw,999,330,100,100);
             Button sth = new Button("6,3");
-            lawnbutton(sth,875,450,120,100);
+            lawnbutton(sth,999,450,100,100);
             Button sf = new Button("6,4");
-            lawnbutton(sf,875,560,120,100);
+            lawnbutton(sf,999,560,100,100);
             Button sez = new Button("7,0");
-            lawnbutton(sez,1010,100,120,100);
+            lawnbutton(sez,1100,100,100,100);
             Button seo = new Button("7,1");
-            lawnbutton(seo,1010,215,120,100);
+            lawnbutton(seo,1100,215,100,100);
             Button setw = new Button("7,2");
-            lawnbutton(setw,1010,330,120,100);
+            lawnbutton(setw,1100,330,100,100);
             Button seth = new Button("7,3");
-            lawnbutton(seth,1010,450,120,100);
+            lawnbutton(seth,1100,450,100,100);
             Button sef = new Button("7,4");
-            lawnbutton(sef,1010,560,120,100);
+            lawnbutton(sef,1100,560,100,100);
             Button ez = new Button("8,0");
-            lawnbutton(ez,1135,100,120,100);
+            lawnbutton(ez,1200,100,100,100);
             Button eo = new Button("8,1");
-            lawnbutton(eo,1135,215,120,100);
+            lawnbutton(eo,1200,215,100,100);
             Button etw = new Button("8,2");
-            lawnbutton(etw,1135,330,120,100);
+            lawnbutton(etw,1200,330,100,100);
             Button eth = new Button("8,3");
-            lawnbutton(eth,1135,450,120,100);
+            lawnbutton(eth,1200,450,100,100);
             Button ef = new Button("8,4");
-            lawnbutton(ef,1135,560,120,100);
+            lawnbutton(ef,1200,560,100,100);
 
             //zz.setStyle("-fx-background-color:transparent;");
             //ng.setOnMouseEntered(e -> ng.setStyle("-fx-background-color: -fx-shadow-highlight-color, -fx-outer-border, -fx-inner-border, -fx-body-color;\""));
@@ -296,10 +319,10 @@ public class Main extends Application {
             stackPaneG.getChildren().addAll(iv1G,pG);
 
             Button pauseMenu = new Button("Menu");
-            pauseMenu.setLayoutX(1150);
+            pauseMenu.setLayoutX(1175);
             pauseMenu.setLayoutY(0);
-            pauseMenu.setPrefSize(170,27);
-            //pauseMenu.setStyle("-fx-background-color:transparent;");
+            pauseMenu.setPrefSize(160,27);
+            pauseMenu.setStyle("-fx-background-color:transparent;");
             pG.getChildren().add(pauseMenu);
             pauseMenu.setOnAction(e->{
                 System.out.println("Menu selected");
@@ -309,13 +332,13 @@ public class Main extends Application {
 
 
             Pane p = new Pane(); //pane with 3 buttons (Main Menu)
-            Image image1 = new Image("MainMenu.jpg", 1350, 800, false, false);
+            Image image1 = new Image("file:MainMenu.jpg", 1350, 800, false, false);
             ImageView iv1 = new ImageView();
             iv1.setImage(image1);
             iv1.setPreserveRatio(true);
 
             Pane p2 = new Pane(); //pane with text box and buttons for new name
-            Image image2 = new Image("Enter_Name.jpg", 500, 300, false, false);
+            Image image2 = new Image("file:Enter_Name.jpg", 500, 300, false, false);
             ImageView iv2 = new ImageView();
             iv2.setX(430);
             iv2.setY(200);
@@ -330,12 +353,15 @@ public class Main extends Application {
             ButtonFormat(NmOk,470,440,190,35);
             NmOk.setOnAction(e -> {
                 System.out.println("OK selected");
+                stackPane.getChildren().remove(p2);
                 stage.setScene(scene2);
+                bullettrans.play();
+                translateTransition.play();
             });
             ButtonFormat(NmCancel,700,440,190,35);
             NmCancel.setOnAction(e -> {
                 System.out.println("Cancel selected");
-                stackPaneG.getChildren().remove(p2);
+                stackPane.getChildren().remove(p2);
 
             });
             p2.getChildren().add(iv2);
@@ -344,7 +370,7 @@ public class Main extends Application {
             p2.getChildren().add(NmCancel);
 
             Pane p3 = new Pane();
-            Image image3 = new Image("ChooseLevel.jpg", 1350, 700, false, false);
+            Image image3 = new Image("file:ChooseLevel.jpg", 1350, 700, false, false);
             ImageView iv3 = new ImageView();
             iv3.setImage(image3);
             p3.getChildren().add(iv3);
