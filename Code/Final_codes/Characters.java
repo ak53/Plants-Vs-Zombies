d1ac39ad18937278d9158acd5115bf104f52af05
deepigarg <48362243+deepigarg@users.noreply.github.com>
2019-11-17 01:09:32 +0530

    Create Characters.java

---
+++ Code/Final_codes/Characters.java
@@ -0,0 +1,23 @@
+package sample;
+
+public abstract class Characters {
+    int Health;
+    int maxHealth;
+    int[] Position;
+
+    public void die(){
+
+    }
+    public int getHealth(){
+        return Health;
+    }
+    public void reduceHealth(int a){
+
+    }
+    public int[] getPosition(){
+        return Position;
+    }
+    public void setPosition(int x, int y){
+
+    }
+}
