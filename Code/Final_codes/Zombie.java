632ce4706a76ae2e30de8056600a77b2b4387ad2
Amandeep Kaur <46717101+thekaur99@users.noreply.github.com>
2019-11-28 19:37:18 +0530

    Update Zombie.java

--- Code/Final_codes/Zombie.java
+++ Code/Final_codes/Zombie.java
@@ -3,6 +3,10 @@ package sample;
 public abstract class Zombie extends Characters{
     int walkSpeed;
 
+    public Zombie(int x,int y,int h,int w,String type) {
+        super(x,y,w,h,"zombie");
+    }
+
     public void Eat(Plant p){
 
     }
@@ -13,16 +17,28 @@ public abstract class Zombie extends Characters{
 
 class BasicZ extends Zombie{
 
+    public BasicZ(int x, int y, int h, int w, String type) {
+        super(x, y, h, w, type);
+    }
 }
 
 class FlagZ extends Zombie{
 
+    public FlagZ(int x, int y, int h, int w, String type) {
+        super(x, y, h, w, type);
+    }
 }
 
 class BucketHeadZ extends Zombie{
 
+    public BucketHeadZ(int x, int y, int h, int w, String type) {
+        super(x, y, h, w, type);
+    }
 }
 
 class ConeHeadZ extends Zombie{
 
+    public ConeHeadZ(int x, int y, int h, int w, String type) {
+        super(x, y, h, w, type);
+    }
 }
