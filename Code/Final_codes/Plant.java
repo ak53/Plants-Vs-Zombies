41bd46b09f2f8e3f6291064ba153d3871801742d
Amandeep Kaur <46717101+thekaur99@users.noreply.github.com>
2019-11-28 19:36:52 +0530

    Update Plant.java

--- Code/Final_codes/Plant.java
+++ Code/Final_codes/Plant.java
@@ -1,14 +1,26 @@
 package sample;
 
+import javafx.scene.paint.Color;
+
 public abstract class Plant extends Characters{
     int reqSun;
     int replantTime;
+
+    public Plant(int x,int y,int h,int w,String type) {
+        super(x,y,h,w,"plant");
+
+    }
+
     public boolean isAvailable(){
         return true;
     }
 }
 
 class Sunflower extends Plant{
+    public Sunflower(int x, int y, int h, int w, String type) {
+        super(x, y, h, w, type);
+    }
+
     public void EmitSun(){
 
     }
@@ -16,9 +28,16 @@ class Sunflower extends Plant{
 
 class Walnut extends Plant{
 
+    public Walnut(int x, int y, int h, int w, String type) {
+        super(x, y, h, w, type);
+    }
 }
 
 class PotatoMine extends Plant{
+    public PotatoMine(int x, int y, int h, int w, String type) {
+        super(x, y, h, w, type);
+    }
+
     public void explode(){
 
     }
@@ -29,11 +48,20 @@ class PotatoMine extends Plant{
 
 class PeaShooter extends Plant{
     int attackStrength;
-    public void Shoot(Zombie z){
 
+    public PeaShooter(int x, int y, int h, int w, String type) {
+        super(x, y, h, w, type);
+    }
+
+    public void shoot(Plant who){
+        Characters c=new Characters((int)who.getTranslateX()+20,(int)who.getTranslateY(),20,5,who.type+"pea");
+        Lawn.root.getChildren().add(c);
     }
 }
 
 class Repeater extends PeaShooter{
 
+    public Repeater(int x, int y, int h, int w, String type) {
+        super(x, y, h, w, type);
+    }
 }
