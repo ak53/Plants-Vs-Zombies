35a1585ea6792706325ff1e59f225cef56df714f
Amandeep Kaur <46717101+thekaur99@users.noreply.github.com>
2019-11-28 19:37:53 +0530

    Update Characters.java

--- Code/Final_codes/Characters.java
+++ Code/Final_codes/Characters.java
@@ -1,15 +1,30 @@
 package sample;
 
-public abstract class Characters {
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class Characters extends Rectangle {
     int Health;
     int maxHealth;
     int[] Position;
+    boolean dead=false;
+    final String type;
+
+    protected Characters(int x, int y, int h, int w, String type) {
+        super(h,w);
+        this.type = type;
+        setTranslateX(x);
+        setTranslateY(y);
+    }
 
     public void die(){
 
     }
     public int getHealth(){
         return Health;
+    }
+    void moveRight(){
+        setTranslateX(getTranslateX()+5);
     }
     public void reduceHealth(int a){
 
@@ -19,5 +34,12 @@ public abstract class Characters {
     }
     public void setPosition(int x, int y){
 
+    }
+}
+
+class Pea extends Characters{
+
+    protected Pea(int x, int y, int h, int w, String type) {
+        super(x, y, h, w, type);
     }
 }
