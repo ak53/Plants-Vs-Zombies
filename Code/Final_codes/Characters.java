92586fb4f772614a9c8e9a8f31796785bbe424a2
Amandeep Kaur <46717101+thekaur99@users.noreply.github.com>
2019-11-29 01:19:33 +0530

    Update Characters.java

--- Code/Final_codes/Characters.java
+++ Code/Final_codes/Characters.java
@@ -1,18 +1,23 @@
 package sample;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
-public class Characters extends Rectangle {
+
+public class Characters extends ImageView {
     int Health;
     int maxHealth;
     int[] Position;
     boolean dead=false;
     final String type;
+    Image i;
 
-    protected Characters(int x, int y, int h, int w, String type) {
-        super(h,w);
+    protected Characters(int x, int y, int h, int w, String type){
+        this.relocate(x,y);
         this.type = type;
+
         setTranslateX(x);
         setTranslateY(y);
     }
@@ -24,7 +29,7 @@ public class Characters extends Rectangle {
         return Health;
     }
     void moveRight(){
-        setTranslateX(getTranslateX()+5);
+        setTranslateX(getTranslateX()+50);
     }
     public void reduceHealth(int a){
 
@@ -41,5 +46,6 @@ class Pea extends Characters{
 
     protected Pea(int x, int y, int h, int w, String type) {
         super(x, y, h, w, type);
+        this.setImage(new Image("Peabullet.png",90,100,false,false));
     }
 }
