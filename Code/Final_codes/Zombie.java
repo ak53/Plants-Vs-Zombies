369fb6bce5725304fc6df42aa4c35a641ab984d4
Amandeep Kaur <46717101+thekaur99@users.noreply.github.com>
2019-11-29 01:20:43 +0530

    Update Zombie.java

--- Code/Final_codes/Zombie.java
+++ Code/Final_codes/Zombie.java
@@ -1,17 +1,37 @@
 package sample;
+import javafx.animation.TranslateTransition;
+import javafx.scene.image.Image;
+import javafx.util.Duration;
+
+import java.util.Random;
 
 public abstract class Zombie extends Characters{
     int walkSpeed;
 
-    public Zombie(int x,int y,int h,int w,String type) {
+    public Zombie(int x, int y, int h, int w, String type) {
         super(x,y,w,h,"zombie");
+    }
+
+    public static Zombie create(){
+        Zombie z=new BasicZ(1250,(new Random().nextInt(5)+1)*150,120,70,"zombie");
+        Lawn.root.getChildren().add(z);
+        return z;
     }
 
     public void Eat(Plant p){
 
     }
     public void Walk(){
-
+        TranslateTransition translateTransition = new TranslateTransition();
+        //Setting the duration of the transition
+        translateTransition.setDuration(Duration.seconds(39));
+        //Setting the node for the transition
+        translateTransition.setNode(this);
+        translateTransition.setFromX(1270);
+//        translateTransition.setFromY(20);
+        //Setting the value of the transition along the x axis.
+        translateTransition.setByX(-910);
+        translateTransition.play();
     }
 }
 
@@ -19,6 +39,7 @@ class BasicZ extends Zombie{
 
     public BasicZ(int x, int y, int h, int w, String type) {
         super(x, y, h, w, type);
+        this.setImage(new Image("Zombie.gif",90,170,false,false));
     }
 }
 
@@ -26,6 +47,8 @@ class FlagZ extends Zombie{
 
     public FlagZ(int x, int y, int h, int w, String type) {
         super(x, y, h, w, type);
+        this.setImage(new Image("flagzombie.gif",90,170,false,false));
+
     }
 }
 
@@ -33,6 +56,8 @@ class BucketHeadZ extends Zombie{
 
     public BucketHeadZ(int x, int y, int h, int w, String type) {
         super(x, y, h, w, type);
+        this.setImage(new Image("buckethead.gif",90,170,false,false));
+
     }
 }
 
@@ -40,5 +65,7 @@ class ConeHeadZ extends Zombie{
 
     public ConeHeadZ(int x, int y, int h, int w, String type) {
         super(x, y, h, w, type);
+        this.setImage(new Image("Conehead.gif",90,170,false,false));
+
     }
 }
